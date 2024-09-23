6e6c61c7eedebf6f29a2817aced32d8d92c1565b
Shlok Singh <134242409+ShlokS2997@users.noreply.github.com>
2024-09-23 05:17:00 -0400

    Update reward.py
    og

--- reward.py
+++ reward.py
@@ -5,9 +5,6 @@ import sys
 import sympy as sp
 import os
 from symbolicregression.metrics import compute_metrics
-from nesymres.src.nesymres.architectures import bfgs
-from sklearn.model_selection import KFold
-
 
 def evaluate_metrics(y_gt, tree_gt, y_pred):
     metrics = []
@@ -25,7 +22,8 @@ def evaluate_metrics(y_gt, tree_gt, y_pred):
     
     return metrics
 
-def compute_reward_e2e(params, samples, y_pred, model_str, generations_tree, n_splits=5):  
+def compute_reward(params,samples, y_pred, model_str, generations_tree):  
+
     # NMSE
     penalty = -2
     if y_pred is None:
@@ -33,58 +31,18 @@ def compute_reward_e2e(params, samples, y_pred, model_str, generations_tree, n_s
     else:
         y = samples['y_to_fit'][0].reshape(-1)
         eps = 1e-9
-        
-        # Cross-Validation Logic
-        kf = KFold(n_splits=n_splits)
-        NMSEs = []
-        
-        for train_index, test_index in kf.split(y):
-            y_train, y_test = y[train_index], y[test_index]
-            y_pred_fold = y_pred[test_index]  # Get predictions for the test fold
-            NMSE = np.sqrt(np.mean((y_test - y_pred_fold) ** 2) / (np.mean(y_test ** 2) + eps))
-            NMSEs.append(NMSE)
+        NMSE = np.sqrt( np.mean((y - y_pred)**2) / (np.mean((y)**2)+eps) ) 
 
-        avg_NMSE = np.mean(NMSEs)
-
-        if not np.isnan(avg_NMSE):
-            reward = 1 / (1 + avg_NMSE)
-        else:
+        if (not np.isnan(NMSE)):
+            reward = 1/(1+NMSE)
+            
+        elif np.isnan(NMSE):
             reward = penalty
 
-        if generations_tree:
+        if generations_tree != []:
             complexity = len(generations_tree[0].prefix().split(","))
+            ### Length penalty
             lam = params.lam
-            reward += lam * np.exp(-complexity / 200)
-
-    return reward
-
-
-def compute_reward_nesymres(X, y, state, cfg_params, n_splits=5):  
-    penalty = -2
-
-    cfg_params.id2word[3] = "constant"
-    
-    # Cross-Validation Logic
-    kf = KFold(n_splits=n_splits)
-    rewards = []
-
-    for train_index, test_index in kf.split(X):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+            reward = reward + lam * np.exp(-complexity/200)
         
-        try:
-            pred_w_c, constants, loss_bfgs, exa = bfgs.bfgs(
-                state, X_train, y_train, cfg_params
-            )
-            if np.isnan(loss_bfgs):
-                rewards.append(penalty)
-            else:
-                lam = 0.1
-                eps = 1e-9
-                nmse = loss_bfgs / (np.mean(y_test.reshape(-1) ** 2) + eps)
-                reward = 1 / (1 + nmse) + lam * np.exp(-(len(state) - 2) / 200)
-                rewards.append(reward)
-        except:
-            rewards.append(penalty)
-
-    return np.mean(rewards), str(pred_w_c)
+    return reward
